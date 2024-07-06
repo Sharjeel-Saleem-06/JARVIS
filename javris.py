f5af19a4e9daeab2d9d8a6699c2aa198e770f2e2
SharrY111 <145919334+SharrY111@users.noreply.github.com>
2024-07-06 13:19:36 +0500

    updated

--- javris.py
+++ javris.py
@@ -4,8 +4,8 @@ import speech_recognition as sr
 import keyboard
 import os
 import subprocess as sp
-# import imdb
-# import wolframalpha
+import imdb
+import wolframalpha
 import pyautogui
 import webbrowser
 import time
@@ -14,26 +14,23 @@ from datetime import datetime
 from decouple import config
 from random import choice
 from conv import random_text
-from online import get_ip_address ,search_on_google, search_on_wikipedia, youtube
-# from online import find_my_ip, search_on_google, search_on_wikipedia, youtube, send_email, get_news, weather_forecast
+from online import find_my_ip, search_on_google, search_on_wikipedia, youtube, send_email, get_news, weather_forecast
 
-# Initialize the text-to-speech engine
 engine = pyttsx3.init('sapi5')
 engine.setProperty('volume', 1.5)
-engine.setProperty('rate', 230)
+engine.setProperty('rate', 220)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-# For username and bot name from config file using decouple
 USER = config('USER')
 HOSTNAME = config('BOT')
 
-# Function to recognize speech
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-# Function for greeting user according to its time
+
 def greet_me():
     hour = datetime.now().hour
     if (hour >= 6) and (hour < 12):
@@ -44,13 +41,15 @@ def greet_me():
         speak(f"Good evening {USER}")
     speak(f"I am {HOSTNAME}. How may i assist you? {USER}")
 
-# Control listening state
+
 listening = False
+
 
 def start_listening():
     global listening
     listening = True
     print("started listening ")
+
 
 def pause_listening():
     global listening
@@ -62,12 +61,11 @@ keyboard.add_hotkey('ctrl+alt+k', start_listening)
 keyboard.add_hotkey('ctrl+alt+p', pause_listening)
 
 
-# Initialize microphone and speech recognizer
 def take_command():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 0.5
+        r.pause_threshold = 1
         audio = r.listen(source)
 
     try:
@@ -75,7 +73,7 @@ def take_command():
         queri = r.recognize_google(audio, language='en-in')
         print(queri)
         if not 'stop' in queri or 'exit' in queri:
-            speak("")
+            speak(choice(random_text))
         else:
             hour = datetime.now().hour
             if hour >= 21 and hour < 6:
@@ -89,7 +87,7 @@ def take_command():
         queri = 'None'
     return queri
 
-# Main calling
+
 if __name__ == '__main__':
     greet_me()
     while True:
@@ -97,38 +95,164 @@ if __name__ == '__main__':
             query = take_command().lower()
             if "how are you" in query:
                 speak("I am absolutely fine sir. What about you")
-            elif "what is pubg" in query:
-                speak("Pubg is a game of stupid people")
 
             elif "open command prompt" in query:
-                speak("Opening Command PRompt")
-                os.system("start cmd")
+                speak("Opening command prompt")
+                os.system('start cmd')
 
             elif "open camera" in query:
-                speak("Opening camera")   
-                sp.run('start microsoft.windows.camera:',shell=True) 
+                speak("Opening camera sir")
+                sp.run('start microsoft.windows.camera:', shell=True)
 
             elif "open notepad" in query:
-                speak("opening notepad for you sir")
-                notepad_path="C:\\Windows\\notepad.exe"
-                os.startfile(notepad_path)    
+                speak("Opening Notepad for you sir")
+                notepad_path = "C:\\Users\\ASUS\\AppData\\Local\\Microsoft\\WindowsApps\\notepad.exe"
+                os.startfile(notepad_path)
 
-            elif "tell me ip address" in query:
-                ip=get_ip_address()
-                speak(f"your ip address is{ip}")
-                
-            elif "youtube"in query:
-                speak("sharry what do you want to play")
-                video=take_command().lower()
+            elif "open discord" in query:
+                speak("Opening Discord for you sir")
+                discord_path = "C:\\Users\\ASUS\\AppData\\Local\\Discord\\app-1.0.9028\\Discord.exe"
+                os.startfile(discord_path)
+
+            elif "open gta" in query:
+                speak("Opening Gta for you sir")
+                gta_path = "D:\\Tanishq\\GTA\\Launcher.exe"
+                os.startfile(gta_path)
+
+            elif 'ip address' in query:
+                ip_address = find_my_ip()
+                speak(
+                    f'Your IP Address is {ip_address}.\n For your convenience, I am printing it on the screen sir.')
+                print(f'Your IP Address is {ip_address}')
+
+            elif "open youtube" in query:
+                speak("What do you want to play on youtube sir?")
+                video = take_command().lower()
                 youtube(video)
 
-            elif "open wikipedia"in query:
-                speak("What do you want to search")
-                query=take_command().lower()
-                search_on_wikipedia(query)   
-                speak("According to wikipedia,{results}")
+            elif "open google" in query:
+                speak(f"What do you want to search on google {USER}")
+                query = take_command().lower()
+                search_on_google(query)
 
-            elif "open google"in query:
-                speak("sharry What do you want to search")
-                query=take_command().lower()
-                search_on_google(query)   
+            elif "wikipedia" in query:
+                speak("what do you want to search on wikipedia sir?")
+                search = take_command().lower()
+                results = search_on_wikipedia(search)
+                speak(f"According to wikipedia,{results}")
+                speak("I am printing in on terminal")
+                print(results)
+
+
+            elif "send an email" in query:
+                speak("On what email address do you want to send sir?. Please enter in the terminal")
+                receiver_add = input("Email address:")
+                speak("What should be the subject sir?")
+                subject = take_command().capitalize()
+                speak("What is the message ?")
+                message = take_command().capitalize()
+                if send_email(receiver_add, subject, message):
+                    speak("I have sent the email sir")
+                    print("I have sent the email sir")
+                else:
+                    speak("something went wrong Please check the error log")
+
+            elif "give me news" in query:
+                speak(f"I am reading out the latest headline of today,sir")
+                speak(get_news())
+                speak("I am printing it on screen sir")
+                print(*get_news(), sep='\n')
+
+            elif 'weather' in query:
+                ip_address = find_my_ip()
+                speak("tell me the name of your city")
+                city = input("Enter name of your city")
+                speak(f"Getting weather report for your city {city}")
+                weather, temp, feels_like = weather_forecast(city)
+                speak(f"The current temperature is {temp}, but it feels like {feels_like}")
+                speak(f"Also, the weather report talks about {weather}")
+                speak("For your convenience, I am printing it on the screen sir.")
+                print(f"Description: {weather}\nTemperature: {temp}\nFeels like: {feels_like}")
+
+            elif "movie" in query:
+                movies_db = imdb.IMDb()
+                speak("Please tell me the movie name:")
+                text = take_command()
+                movies = movies_db.search_movie(text)
+                speak("searching for" + text)
+                speak("I found these")
+                for movie in movies:
+                    title = movie["title"]
+                    year = movie["year"]
+                    speak(f"{title}-{year}")
+                    info = movie.getID()
+                    movie_info = movies_db.get_movie(info)
+                    rating = movie_info["rating"]
+                    cast = movie_info["cast"]
+                    actor = cast[0:5]
+                    plot = movie_info.get('plot outline', 'plot summary not available')
+                    speak(f"{title} was released in {year} has imdb ratings of {rating}.It has a cast of {actor}. "
+                          f"The plot summary of movie is {plot}")
+
+                    print(f"{title} was released in {year} has imdb ratings of {rating}.\n It has a cast of {actor}. \n"
+                          f"The plot summary of movie is {plot}")
+
+
+            elif "calculate" in query:
+                app_id = ""
+                client = wolframalpha.Client(app_id)
+                ind = query.lower().split().index("calculate")
+                text = query.split()[ind + 1:]
+                result = client.query(" ".join(text))
+                try:
+                    ans = next(result.results).text
+                    speak("The answer is " + ans)
+                    print("The answer is " + ans)
+                except StopIteration:
+                    speak("I couldn't find that . Please try again")
+
+
+            # elif 'what is' in query or 'who is' in query or 'which is' in query:
+            #     app_id = ""
+            #     client = wolframalpha.Client(app_id)
+            #     try:
+
+            #         ind = query.lower().index('what is') if 'what is' in query.lower() else \
+            #             query.lower().index('who is') if 'who is' in query.lower() else \
+            #                 query.lower().index('which is') if 'which is' in query.lower() else None
+
+            #         if ind is not None:
+            #             text = query.split()[ind + 2:]
+            #             res = client.query(" ".join(text))
+            #             ans = next(res.results).text
+            #             speak("The answer is " + ans)
+            #             print("The answer is " + ans)
+            #         else:
+            #             speak("I couldn't find that. Please try again.")
+            #     except StopIteration:
+            #         speak("I couldn't find that. Please try again.")
+
+            elif 'subscribe' in query:
+                speak(
+                    "Everyone who are watching this video, Please subscribe for more amazing content from error by "
+                    "night. I will show you how to do this")
+                speak("Firstly Go to youtube")
+                webbrowser.open("https://www.youtube.com/")
+                speak("click on the search bar")
+                pyautogui.moveTo(806, 125, 1)
+                pyautogui.click(x=806, y=125, clicks=1, interval=0, button='left')
+                speak("Error by night")
+                pyautogui.typewrite("Error by night", 0.1)
+                time.sleep(1)
+                speak("press enter")
+                pyautogui.press('enter')
+                pyautogui.moveTo(971, 314, 1)
+                speak("Here you will see our channel")
+                pyautogui.moveTo(1688, 314, 1)
+                speak("click here to subscribe our channel")
+                pyautogui.click(x=1688, y=314, clicks=1, interval=0, button='left')
+                speak("And also Don't forget to press the bell icon")
+                pyautogui.moveTo(1750, 314, 1)
+                pyautogui.click(x=1750, y=314, clicks=1, interval=0, button='left')
+                speak("turn on all notifications")
+                pyautogui.click(x=1750, y=320, clicks=1, interval=0, button='left')
